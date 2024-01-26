43409790e7299f8e2e945f077e19489970ecd460
Ritik <ritikjaiswal148@gmail.com>
2024-01-26 11:16:47 +0530

    app demo

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+
+// import MintButton from "./mintButton";
+// import SendButton from "./sendButton";
+import WalletButton from "./walletbutton";
+
+export default function Home() {
+      return (
+            <main className="min-h-screen">
+                  <div className="flex flex-col items-center justify-center h-screen ">
+                        <a href="https://rareskills.io" target="_blank" className="text-white font-bold text-3xl hover:text-[#0044CC]" > Viem.sh </a>
+                        <div className="h-[300px] min-w-[150px] flex flex-col justify-between  backdrop-blur-2xl bg-[#290330]/30 rounded-lg mx-auto p-7 text-white border border-purple-950">                                                                     
+                              <WalletButton />
+                              {/* <SendButton />
+                              <MintButton /> */}
+                        </div>
+                        <a href="https://rareskills.io" target="_blank" className="text-white font-bold text-3xl hover:text-[#0044CC]" > Rareskills.io </a>
+                 </div>
+            </main>
+      );
+}
